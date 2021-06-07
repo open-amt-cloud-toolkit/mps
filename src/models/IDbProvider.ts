a60995a1f0947db19f8dd1f4a7354b34413b3240
madhavi-losetty <madhavi.losetty@intel.com>
2021-06-07 12:34:15 -0700

    fix(config): removed use_allowlist from config

--- src/models/IDbProvider.ts
+++ src/models/IDbProvider.ts
@@ -1,4 +1,3 @@
-import { Credentials } from './models'
 
 /*********************************************************************
 * Copyright (c) Intel Corporation 2020
@@ -8,6 +7,4 @@ export interface IDbProvider {
   CIRAAuth: (guid: string, username: string, password: string, cb: any) => any
   getAmtPassword: (uuid: string) => any
   IsGUIDApproved: (guid: string, cb: any) => any
-  IsOrgApproved: (org: string, cb: any) => any
-  getAllAmtCredentials: () => Credentials | Promise<Credentials>
 }
