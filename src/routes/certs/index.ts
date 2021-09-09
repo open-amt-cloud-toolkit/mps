d9e04e399616b0dc5b79578aa75f7d7a52412f6d
Mike <mjohanson@gesdate.com>
2021-09-09 11:40:14 -0700

    fix(api): cert now pulls from config instead of disk
    closes #294

--- src/routes/certs/index.ts
+++ src/routes/certs/index.ts
@@ -4,18 +4,15 @@
  **********************************************************************/
 import { logger as log } from '../../utils/logger'
 import { Request, Response } from 'express'
-import * as fs from 'fs'
-import * as path from 'path'
 import { ErrorResponse } from '../../utils/amtHelper'
 
 export async function mpsrootcert (req: Request, res: Response): Promise<void> {
   try {
-    const certPath = path.join(__dirname, '../../../private/root-cert-public.crt')
-    if (fs.existsSync(certPath)) {
-      res.send(fs.readFileSync(certPath))
-    } else {
+    if (req.mpsService.certs?.web_tls_config?.ca == null) {
       res.status(500).json(ErrorResponse(500, 'MPS root certificate does not exists.')).end()
+      return
     }
+    res.send(req.mpsService.certs.web_tls_config.ca)
   } catch (error) {
     log.error(`Exception while downloading MPS root certificate: ${error}`)
     res.status(500).json(ErrorResponse(500, 'Request failed while downloading MPS root certificate.')).end()
