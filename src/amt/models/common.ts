977ba3d49eb55eb857713cdfb66c27bacc58881d
madhavilosetty-intel <madhavi.losetty@intel.com>
2021-12-08 10:09:07 -0700

    refactor(api): get version uses new amt libraries (#446)
    AB#4548
    
    Co-authored-by: Mike <michael.johanson@intel.com>

--- src/amt/models/common.ts
+++ src/amt/models/common.ts
@@ -1,16 +1,34 @@
 export interface Header {
-  Header: {
-    To: string
-    RelatesTo: string
-    Action: string
-    MessageID: string
-    ResourceURI: string
-  }
+  To: string
+  RelatesTo: string
+  Action: string
+  MessageID: string
+  ResourceURI: string
 }
 
 export interface DigestChallenge {
   realm?: string
   nonce?: string // Uniquely generated everytime a 401 response made
-  state?: boolean
+  stale?: string
   qop?: string // quality of protection
+}
+
+export interface Response<T> {
+  Envelope: {
+    Header: Header
+    Body: T
+  }
+}
+
+export interface Enumerate {
+  EnumerateResponse: {
+    EnumerationContext: string
+  }
+}
+
+export interface Pull<T> {
+  PullResponse: {
+    Items: T
+    EndOfSequence: string
+  }
 }
--- src/routes/amt/version.ts
+++ src/routes/amt/version.ts
@@ -13,19 +13,14 @@ import { devices } from '../../server/mpsserver'
 export async function version (req: Request, res: Response): Promise<void> {
   try {
     const guid: string = req.params.guid
-    // req.amtStack.BatchEnum('', ['CIM_SoftwareIdentity', '*AMT_SetupAndConfigurationService'],
-    //   (stack, name, responses, status) => {
-    //     stack.wsman.comm.socket.sendchannelclose()
-    //     if (status === 200) {
-    //       res.status(200).json(responses).end()
-    //     } else {
-    //       log.error(`Request failed during AMTVersion BatchEnum Exec for guid : ${guid}.`)
-    //       res.status(status).json(ErrorResponse(status, `Request failed during AMTVersion BatchEnum Exec for guid : ${guid}.`)).end()
-    //     }
-    //   })
     MqttProvider.publishEvent('request', ['AMT_Version'], 'Power State Requested', guid)
     const response = await devices[guid].getVersion()
-    res.status(200).send(response)
+    if (response != null) {
+      res.status(200).json(response).end()
+    } else {
+      log.error(`Request failed during AMTVersion BatchEnum Exec for guid : ${guid}.`)
+      res.status(400).json(ErrorResponse(400, `Request failed during AMTVersion BatchEnum Exec for guid : ${guid}.`)).end()
+    }
   } catch (error) {
     log.error(`Exception in AMT Version : ${error}`)
     res.status(500).json(ErrorResponse(500, 'Request failed during AMTVersion BatchEnum Exec.')).end()
