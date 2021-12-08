30718669b77487c36adbb0a9df0290ed6917c6ef
Matt Primrose <47540908+matt-primrose@users.noreply.github.com>
2021-12-08 13:28:14 -0700

    refactor(amt_models): matching case (#453)

--- src/amt/models/ips_models.ts
+++ src/amt/models/ips_models.ts
@@ -6,18 +6,18 @@
 import { CIM_SecurityService, CIM_Service } from './cim_models'
 
 export interface IPS_HostBasedSetupService extends CIM_SecurityService<IPS_HostBasedSetupService> {
-  currentControlMode?: number
-  allowedControlModes?: number[]
-  configurationNonce?: number[]
-  certChainStatus?: number
+  CurrentControlMode?: number
+  AllowedControlModes?: number[]
+  ConfigurationNonce?: number[]
+  CertChainStatus?: number
 }
 export interface IPS_OptInService extends CIM_Service<IPS_OptInService> {
-  optInCodeTimeout?: number
-  optInRequired?: number
-  optInState?: number
-  canModifyOptInPolicy?: number
-  optInDisplayTimeout?: number
-  startOptIn?: () => number
-  cancelOptIn?: () => number
-  sendOptInCode?: (optInCode: number) => number
+  OptInCodeTimeout?: number
+  OptInRequired?: number
+  OptInState?: number
+  CanModifyOptInPolicy?: number
+  OptInDisplayTimeout?: number
+  StartOptIn?: () => number
+  CancelOptIn?: () => number
+  SendOptInCode?: (optInCode: number) => number
 }
