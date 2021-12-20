5124aa1c5b924c3951b44653f2879d5d5770c2d0
madhavilosetty-intel <madhavi.losetty@intel.com>
2021-12-20 14:28:30 -0800

    fix(api): updated return value code with value type

--- src/routes/amt/getVersion.ts
+++ src/routes/amt/getVersion.ts
@@ -14,15 +14,40 @@ export async function version (req: Request, res: Response): Promise<void> {
   try {
     const guid: string = req.params.guid
     MqttProvider.publishEvent('request', ['AMT_Version'], 'Power State Requested', guid)
-    const response = await devices[guid].getVersion()
-    if (response != null) {
-      res.status(200).json(response)
-    } else {
+    const response = await getVersion(guid)
+    if (response == null) {
       log.error(`Request failed during AMTVersion BatchEnum Exec for guid : ${guid}.`)
       res.status(400).json(ErrorResponse(400, `Request failed during AMTVersion BatchEnum Exec for guid : ${guid}.`))
+    } else {
+      res.status(200).json(response)
     }
   } catch (error) {
     log.error(`Exception in AMT Version : ${error}`)
     res.status(500).json(ErrorResponse(500, 'Request failed during AMTVersion BatchEnum Exec.'))
+  }
+}
+
+// matches version 2.x API for Open AMT
+export async function getVersion (guid: string): Promise<any> {
+  const response: {[key: string]: any} = {}
+  response.CIM_SoftwareIdentity = await devices[guid].getSoftwareIdentity()
+  response.AMT_SetupAndConfigurationService = await devices[guid].getSetupAndConfigurationService()
+  if (Object.values(response).some(item => item == null)) {
+    return null
+  } else {
+    return {
+      CIM_SoftwareIdentity: {
+        responses: response.CIM_SoftwareIdentity?.Envelope?.Body?.PullResponse?.Items?.CIM_SoftwareIdentity,
+        status: 200
+      },
+      AMT_SetupAndConfigurationService: {
+        response: response.AMT_SetupAndConfigurationService?.Envelope?.Body?.AMT_SetupAndConfigurationService,
+        responses: {
+          Header: response.AMT_SetupAndConfigurationService.Envelope?.Header,
+          Body: response.AMT_SetupAndConfigurationService.Envelope?.Body?.AMT_SetupAndConfigurationService
+        },
+        status: 200
+      }
+    }
   }
 }
