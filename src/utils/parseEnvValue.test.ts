ce732527e73fd29d0e72ac7744e597d163c4c4e1
Jesus Olan <jesus.olan@intel.com>
2022-01-17 15:42:14 -0600

    test(utils): Adding parseEnvValue unit test
    Adding unit testing to utils/parseEnvValue.ts
    
    AB#5550

---
+++ src/utils/parseEnvValue.test.ts
@@ -0,0 +1,32 @@
+import * as parse from './parseEnvValue'
+
+describe('Check parseEnvValue', () => {
+  it('Should pass when converting string to number', async () => {
+    const result = await parse.parseValue('1020')
+    expect(result).toEqual(1020)
+  })
+  it('Should pass when passing number', async () => {
+    const result = await parse.parseValue(12345)
+    expect(result).toEqual(12345)
+  })
+  it('Should pass when converting string to true', async () => {
+    const result = await parse.parseValue('true')
+    expect(result).toEqual(true)
+  })
+  it('Should pass when converting string to false', async () => {
+    const result = await parse.parseValue('false')
+    expect(result).toEqual(false)
+  })
+  it('Should pass when converting FALSE to false', async () => {
+    const result = await parse.parseValue('FALSE')
+    expect(result).toEqual(false)
+  })
+  it('Should pass when converting TRUE to true', async () => {
+    const result = await parse.parseValue('TRUE')
+    expect(result).toEqual(true)
+  })
+  it('Should return val', async () => {
+    const result = await parse.parseValue('fake')
+    expect(result).toEqual('fake')
+  })
+})
