63ce782f248cf222204132b76e5cbfc2728c07a6
Barney Cheng <barney.cheng@intel.com>
2022-01-18 10:24:32 -0800

    test(devices): adds tests for devices route
    closes AB#5499

--- src/routes/devices/disconnect.ts
+++ src/routes/devices/disconnect.ts
@@ -10,19 +10,9 @@ import { devices } from '../../server/mpsserver'
 export async function disconnect (req: Request, res: Response): Promise<void> {
   try {
     const guid = req.params.guid
-    // check if guid is connected
     const device = devices[guid]
-    if (device) {
-      try {
-        device.ciraSocket.destroy()
-        res.json({ success: 200, description: `CIRA connection disconnected : ${guid}` })
-      } catch (error) {
-        log.error(`cira connection destroy exception: ${error}`)
-        res.status(500).json(ErrorResponse(500, error))
-      }
-    } else {
-      res.status(404).json(ErrorResponse(404, `guid : ${guid}`, 'device'))
-    }
+    device.ciraSocket.destroy()
+    res.json({ success: 200, description: `CIRA connection disconnected : ${guid}` })
   } catch (error) {
     log.error(`Exception in Disconnect: ${JSON.stringify(error)} `)
     res.status(500).json(ErrorResponse(500, 'Request failed while disconnecting device.'))
