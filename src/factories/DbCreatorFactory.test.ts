ef86d0d9ca90eb96492e2081f5060e58d368111b
Jesus Olan <jesus.olan@intel.com>
2022-01-24 10:33:24 -0600

    test(factories): Adding unit test to factories
    Adding new files for unit testing factories content
    
    AB#5589

---
+++ src/factories/DbCreatorFactory.test.ts
@@ -0,0 +1,30 @@
+import PostgresDb from '../data/postgres'
+import { Environment } from '../utils/Environment'
+import { DbCreatorFactory } from './DbCreatorFactory'
+
+describe('', () => {
+  let dbCreator: DbCreatorFactory
+  beforeEach(() => {
+    jest.clearAllMocks()
+    dbCreator = new DbCreatorFactory()
+    Environment.Config = {
+      db_provider: 'postgres',
+      connection_string: ''
+    } as any
+  })
+  it('should pass with default connection string', async () => {
+    Environment.Config.connection_string = 'postgresql://<USERNAME>:<PASSWORD>@localhost:5432/mpsdb?sslmode=no-verify'
+    DbCreatorFactory.instance = null
+    const response = await dbCreator.getDb()
+    const provider = await import(`../data/${Environment.Config.db_provider}`)
+    // eslint-disable-next-line new-cap
+    const expected = new provider.default(Environment.Config.connection_string)
+    expect(typeof response).toEqual(typeof expected)
+  })
+  it('should pass returning an instance of IDB', async () => {
+    Environment.Config.connection_string = 'postgresql://<USERNAME>:<PASSWORD>@localhost:5432/mpsdb?sslmode=no-verify'
+    DbCreatorFactory.instance = new PostgresDb(Environment.Config.connection_string)
+    const response = await dbCreator.getDb()
+    expect(typeof response).toEqual(typeof DbCreatorFactory.instance)
+  })
+})
