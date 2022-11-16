74990b9614090979136a9be880266b930040e823
Walt M <walter.a.morton@intel.com>
2022-11-15 17:09:24 -0800

    refactor(cira): increases stability of calls to a device
    add throttling of requests to a single device by use of the npm package 'Bottleneck'
    
    AB#14657

--- src/amt/ConnectedDevice.ts
+++ src/amt/ConnectedDevice.ts
@@ -3,17 +3,23 @@
  * SPDX-License-Identifier: Apache-2.0
  **********************************************************************/
 
+import Bottleneck from 'bottleneck'
 import { CIRASocket } from '../models/models'
 import { HttpHandler } from './HttpHandler'
 
 export class ConnectedDevice {
   httpHandler: HttpHandler
   ciraSocket: CIRASocket
+  limiter: Bottleneck
   kvmConnect: boolean
 
   constructor (ciraSocket: CIRASocket, readonly username: string, readonly password: string) {
     this.ciraSocket = ciraSocket
     this.httpHandler = new HttpHandler()
     this.kvmConnect = false
+    this.limiter = new Bottleneck({
+      maxConcurrent: 3,
+      minTime: 250
+    })
   }
 }
