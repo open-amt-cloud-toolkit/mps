bb27bd1b3165c9ee41ea0a4c5efe83489a84efaf
Mike Johanson <michael.johanson@intel.com>
2023-06-21 10:29:12 -0700

    fix: default tenantId is now blank
    get device should return on authenticated matching tenant

--- src/middleware/custom/example.ts
+++ src/middleware/custom/example.ts
@@ -15,7 +15,8 @@ exports = module.exports = function (req, res, next) {
   // req.db.customProperty = req.headers['x-db-info']
 
   // For setting the tenantId use req.tenantId
-  // req.tenantId = req.headers['x-tenant-id-token']
+  // req.tenantId = req.headers['x-tenant-id-token'] // pull tenant id from header
+  req.tenantId = '' // default blank tenant
   // Be sure to reject requests that do not have access to the tenant
   //   if (req.tenantId === <resource's>.tenantId) {
   //     next()
